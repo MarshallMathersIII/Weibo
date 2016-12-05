fb94ea99e75e4d5a2595298946f5001c6709a2da
gonghongyu <gonghongyu@ftzex.com>
2016-12-05 17:55:52 +0800

    5：SplashActivity

---
+++ app/src/main/java/com/eminem/weibo/activity/SplashActivity.java
@@ -0,0 +1,50 @@
+package com.eminem.weibo.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import com.eminem.weibo.BaseActivity;
+import com.eminem.weibo.R;
+import com.eminem.weibo.constants.AccessTokenKeeper;
+import com.sina.weibo.sdk.auth.Oauth2AccessToken;
+
+public class SplashActivity extends BaseActivity {
+    private Oauth2AccessToken accessToken;
+    private static final int WHAT_INTENT2LOGIN = 1;
+    private static final int WHAT_INTENT2MAIN = 2;
+    private static final long SPLASH_DUR_TIME = 1000;
+
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case WHAT_INTENT2MAIN:
+                    intent2Activity(MainActivity.class);
+                    finish();
+                    break;
+                case WHAT_INTENT2LOGIN:
+                    intent2Activity(LoginActivity.class);
+                    finish();
+                    break;
+            }
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        accessToken = AccessTokenKeeper.readAccessToken(this);
+        if (accessToken.isSessionValid()) {
+            handler.sendEmptyMessageDelayed(WHAT_INTENT2MAIN, SPLASH_DUR_TIME);
+        } else {
+            handler.sendEmptyMessageDelayed(WHAT_INTENT2LOGIN, SPLASH_DUR_TIME);
+        }
+
+
+    }
+}
